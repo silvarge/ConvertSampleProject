5aea798bf8eb77b1a21e2588229fd26d585a0a03
silverage <sep.non2@gmail.com>
2024-07-31 23:53:56 +0900

    navigation and fragment connect

---
+++ app/src/main/java/com/example/convertsampleproj/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.convertsampleproj
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.convertsampleproj.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+  //  binding 해주기 (뷰 바인딩 설정)
+  private val binding: ActivityMainBinding by lazy {
+    ActivityMainBinding.inflate(layoutInflater)
+  }
+  
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContentView(binding.root)
+    
+    setBottomNavigationView()
+    
+    //    하단 Navigation 바 추가, Padding 조절 (Bottom: 0)
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+      insets
+    }
+  }
+  
+  fun setBottomNavigationView() {
+    binding.bottomNavigation.setOnItemSelectedListener { item ->
+      when (item.itemId) {
+        R.id.navigation_convert -> {
+          supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, ConvertFragment()).commit()
+          true
+        }
+        
+        R.id.navigation_table -> {
+          supportFragmentManager.beginTransaction().replace(R.id.main_container, TableFragment())
+            .commit()
+          true
+        }
+        
+        R.id.navigation_disclaimer -> {
+          supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, DisclaimerFragment()).commit()
+          true
+        }
+        
+        R.id.navigation_contact -> {
+          supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, ContactFragment()).commit()
+          true
+        }
+        
+        else -> false
+      }
+    }
+  }
+}
